0a8aaef8faab0650207fc9540572160d6524676e
Cody Phillips <codyphillips@area30.k12.in.us>
2019-04-30 10:45:03 -0400

    another one

--- script.js
+++ script.js
@@ -119,33 +119,37 @@ var submitButton = document.getElementById('submit');
 
 
 
-var countDownDate = new Date("june 25, 2020 5:37:25").getTime();
+
 
 
 var x = setInterval(function() {
-
+	var countDownDate = new Date("june 25, 2020 5:37:25").getTime();
  
   var now = new Date().getTime();
     
   
   var distance = countDownDate - now;
     
-  
+	
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-  
   document.getElementById("demo").innerHTML = days + "d" + hours + "h"
   + minutes + "m" + seconds + "s";
-    
+	
+  
+	
   
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
+		document.getElementById("demo").innerHTML = "EXPIRED";
+	
+		
+	}
+}, 
+1000);
 
 
 
